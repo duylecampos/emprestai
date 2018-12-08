7b4cdbf8f9525fcdd29abf8b66af58d582d9ea81
yonaspassos <yona.nantes22@gmail.com>
2018-12-08 21:10:42 -0200

    feat: implementado Topbar
    Foi adicionado o avatar do usuario logado e o botão de adicionar. Além disso, todos os elementos foram estilizados.

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+import classes from './Topbar.module.css';
 
 const topbar = props => {
-    return <input type="text" />;
+    return <header class={classes.Topbar}>
+        <div class={classes.avatar}>
+            <img src="https://www.fakepersongenerator.com/Face/female/female20161024562624786.jpg" alt="Foto do usuário" />
+        </div>
+        <div class={classes.search}>
+            <input type="text" name="search" placeholder="Busque seu livro" />                
+        </div>
+        <div class={classes.add}>
+            <button><FontAwesomeIcon icon={faPlus} /></button>
+
+        </div>
+    </header>;
 }
 
 export default topbar;
